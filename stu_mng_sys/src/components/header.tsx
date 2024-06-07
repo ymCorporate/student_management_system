b3e6b8fdfdcb67d5037b2be70fb613e9cb19073e
yashas <yashas.b@talview.com>
2024-06-07 13:47:42 +0530

    Header

---
+++ stu_mng_sys/src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header: React.FC = ()=>(
+  <header className="bg-blue-600 text-white p-4">
+    <h1 className="text-2xl">Student Management System</h1>
+  </header>
+);
+
+export default Header;
