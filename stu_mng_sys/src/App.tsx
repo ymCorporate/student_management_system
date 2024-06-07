a452676eee79c2fa85ecbffccfafe350c8f23576
yashas <yashas.b@talview.com>
2024-06-07 20:11:57 +0530

    added footer

--- stu_mng_sys/src/App.tsx
+++ stu_mng_sys/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import StudentList from './components/list';
+import Footer from './components/footer';
 
 const App: React.FC=()=>{
   return (
     <div className="App">
       <Header/>
       <StudentList/>
+      <Footer />
     </div>
   );
 }
---
+++ stu_mng_sys/src/components/footer.tsx
@@ -0,0 +1,10 @@
+// src/components/Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="bg-blue-600 text-white p-4 mt-4">
+    <p>Copyright 2024 Student Management System</p>
+  </footer>
+);
+
+export default Footer;
